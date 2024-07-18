3a96210d1105af22bceda374f80c0ba6ce563572
eltonplima <me@eltonplima.dev>
2024-07-18 03:06:23 +0100

    Add fallback controller

--- lib/graceful_shutdown_demo_web/controllers/slow_math_controller.ex
+++ lib/graceful_shutdown_demo_web/controllers/slow_math_controller.ex
@@ -1,8 +1,12 @@
 defmodule GracefulShutdownDemoWeb.SlowMathController do
   use GracefulShutdownDemoWeb, :controller
 
+  action_fallback(GracefulShutdownDemoWeb.FallbackController)
+
   def sum(conn, %{"version" => version, "numbers" => numbers}) do
-   json(conn, %{result: slow_sum(version, numbers)})
+    with {:ok, result} <- slow_sum(version, numbers) do
+      json(conn, %{result: result})
+    end
   end
 
   defp slow_sum("1", numbers) do
@@ -10,5 +14,5 @@ defmodule GracefulShutdownDemoWeb.SlowMathController do
     Enum.sum(numbers)
   end
 
-  defp slow_sum(_, _), do: {:error, "Unsupported version"}
+  defp slow_sum(_, _), do: {:error, :unsupported_version}
 end
