e612c03762cc4540207cd27f4d14deb729ffc165
eltonplima <me@eltonplima.dev>
2024-07-22 05:22:45 +0100

    style: mix format

--- test/graceful_shutdown_demo_web/controllers/error_json_test.exs
+++ test/graceful_shutdown_demo_web/controllers/error_json_test.exs
@@ -2,7 +2,9 @@ defmodule GracefulShutdownDemoWeb.ErrorJSONTest do
   use GracefulShutdownDemoWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert GracefulShutdownDemoWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert GracefulShutdownDemoWeb.ErrorJSON.render("404.json", %{}) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500" do
